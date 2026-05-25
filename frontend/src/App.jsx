8c5e4fcba702d2a4c1d542c1e13cd4e2c22119d1
Gopika Agrawal <gopikagoyal3336@gmail.com>
2026-05-26 01:50:40 +0530

    Initial frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div>
+      <Button>Click</Button>
+      <Button>Show</Button>
+    </div>
+  )
+}
+
+export default App
